144d51747cdf31e5e96ec85e3f79bfdf9ee1c145
pindardev <pindardev@gmail.com>
2015-01-24 19:34:41 -0500

    Added routes

--- js/app.js
+++ js/app.js
@@ -1,1 +1,30 @@
-var app = angular.module('mavrck', []);
+var app = angular.module('mavrck', ['ngRoute']);
+app.config(['$routeProvider',
+  function($routeProvider) {
+    $routeProvider.
+      when('/login', {
+        templateUrl: 'templates/login.html',
+        controller: 'LoginCtrl'
+      }).
+      when('/product', {
+        templateUrl: 'templates/products_list.html',
+        controller: 'ProductCtrl'
+      }).
+      when('/product/:productId', {
+        templateUrl: 'templates/product_detail.html',
+        controller: 'ProductCtrl'
+      }).
+      otherwise({
+        redirectTo: '/login'
+      });
+  }]);
+
+app.controller('LoginCtrl', ['$scope', function($scope) {
+
+
+}]);
+
+app.controller('ProductCtrl', ['$scope', function($scope) {
+
+
+}]);
